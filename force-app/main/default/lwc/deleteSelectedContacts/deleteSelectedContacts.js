57e1503f14f3df022e0a37070215a89eb8fc9f71
InnaKamenskaya <kamenskayainn@gmail.com>
2020-12-01 21:54:11 +0200

    request object type was changed from string list to contact list, pagination was added

--- force-app/main/default/lwc/deleteSelectedContacts/deleteSelectedContacts.js
+++ force-app/main/default/lwc/deleteSelectedContacts/deleteSelectedContacts.js
@@ -4,6 +4,7 @@ import start from '@salesforce/apex/ContactController.start';
 
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import{ refreshApex } from '@salesforce/apex';
+
 const COLUMNS = [
     {label: 'FirstName', fieldName: 'FirstName', type: 'text'},
     {label: 'LastName', fieldName: 'LastName', type: 'text'},
@@ -16,38 +17,42 @@ export default class DeleteSelectedContacts extends LightningElement {
     @track columns = COLUMNS;
     @track data = [];
     @track recordsCount = 0;
+    @track page = 1;
+    @track pages = [];
+    perpage = 2;
     selectedRecords = [];
     refreshTable;
     error;
 
-    @wire(getAllContacts)
-    contacts(result) {
-        this.refreshTable = result;
-        if(result.data){
-            this.data = result.data;
-            this.error = undefined;
-        }else if (result.error){
-            this.error = result.error;
-            this.data = undefined;
-        }
+    async connectedCallback(){
+        this.data = await getAllContacts();
+        this.setPages(this.data);
+        
     }
-    
+
     getSelectedRecords(event) {
-        const selectedRows = event.detail.selectedRows;
+        let selectedRows = event.detail.selectedRows;
         this.recordsCount = event.detail.selectedRows.length;
         let contactSet = new Set();
         for (let i = 0; i < selectedRows.length; i++) {
-            window.console.log(selectedRows[i].Id + ' ' + selectedRows[i].AccountId);
-            contactSet.add(selectedRows[i].Id + ' ' + selectedRows[i].AccountId);            
+            let contact = {              
+                Id : selectedRows[i].Id,
+                AccountId: selectedRows[i].AccountId,
+                attributes: {
+                    type: "Contact"
+                }                
+            };
+           contactSet.add(contact);          
         }
         if(contactSet){
             this.selectedRecords = Array.from(contactSet);
         }
+            
         window.console.log('selectedRecords ====> ' + this.selectedRecords);
     }
 
-    deleteAll() {
-        start({frontSource: this.selectedRecords})
+    deleteAll() {       
+        start({frontSource: JSON.stringify(this.selectedRecords)})
         .then(result => {
             window.console.log('result ====> ' + result.body);
             this.dispatchEvent(
@@ -76,4 +81,48 @@ export default class DeleteSelectedContacts extends LightningElement {
             );
         });
     }
+
+
+    get pagesList(){
+        let mid = Math.floor(this.set_size/2) + 1 ;
+        if(this.page > mid){
+            return this.pages.slice(this.page-mid, this.page+mid-1);
+        } 
+        return this.pages.slice(0,this.set_size);
+    }
+    
+    pageData = ()=>{
+        let page = this.page;
+        let perpage = this.perpage;
+        let startIndex = (page*perpage) - perpage;
+        let endIndex = (page*perpage);
+        return this.data.slice(startIndex,endIndex);
+    }
+
+    setPages = (data)=>{
+        let numberOfPages = Math.ceil(data.length / this.perpage);
+        for (let index = 1; index <= numberOfPages; index++) {
+            this.pages.push(index);
+        }
+    }  
+    
+    get hasPrev(){
+        return this.page > 1;
+    }
+    
+    get hasNext(){
+        return this.page < this.pages.length
+    }
+
+    handleNextPage = ()=>{
+        ++this.page;
+    }
+
+    handlePrevPage = ()=>{
+        --this.page;
+    }
+
+    get currentPageData(){
+        return this.pageData();
+    }  
 }
